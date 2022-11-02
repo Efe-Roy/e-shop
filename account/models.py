c301d829337ee84568c443023a1e57dc4b608bc9
Efe-Roy <dakaraefe2@gmail.com>
2022-11-02 11:49:51 +0100

    second commit

--- account/models.py
+++ account/models.py
@@ -10,7 +10,7 @@ class User(AbstractUser):
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_of_birth = models.DateField(blank=True, null=True)
-    photo = models.ImageField(upload_to='static/users/%Y/%m/%d/', blank=True)
+    photo = models.ImageField(upload_to='static/users/%Y/%m/%d/', blank=True, null= True)
     
     def __str__(self):
         return f'Profile for user {self.user.username}'
